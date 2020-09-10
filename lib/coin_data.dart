aa7237041cd8d1aab5fe96571cd2c3f12013bdda
itsmordecai <mordecai.kipngetich@gmail.com>
2020-09-10 10:37:45 +0300

    update: refactor

--- lib/coin_data.dart
+++ lib/coin_data.dart
@@ -38,10 +38,8 @@ const apiKey = 'D9C23202-2915-4672-8F5F-790A4B186EAC';
 class CoinData {
   //TODO 3: Update getCoinData to take the selectedCurrency as an input.
   Future getCoinData(String selectedCurrency) async {
-    final String currency = selectedCurrency;
-
     //TODO 4: Update the URL to use the selectedCurrency input.
-    String requestURL = '$coinAPIURL/BTC/$currency?apikey=$apiKey';
+    String requestURL = '$coinAPIURL/BTC/$selectedCurrency?apikey=$apiKey';
     http.Response response = await http.get(requestURL);
     if (response.statusCode == 200) {
       var decodedData = jsonDecode(response.body);
