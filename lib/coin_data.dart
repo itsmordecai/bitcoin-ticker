63fa13184d7dc2e32f9f9404ba35f9d240083fef
itsmordecai <mordecai.kipngetich@gmail.com>
2020-09-10 10:33:59 +0300

    update: silver level complete

--- lib/coin_data.dart
+++ lib/coin_data.dart
@@ -37,9 +37,11 @@ const apiKey = 'D9C23202-2915-4672-8F5F-790A4B186EAC';
 
 class CoinData {
   //TODO 3: Update getCoinData to take the selectedCurrency as an input.
-  Future getCoinData() async {
+  Future getCoinData(String selectedCurrency) async {
+    final String currency = selectedCurrency;
+
     //TODO 4: Update the URL to use the selectedCurrency input.
-    String requestURL = '$coinAPIURL/BTC/USD?apikey=$apiKey';
+    String requestURL = '$coinAPIURL/BTC/$currency?apikey=$apiKey';
     http.Response response = await http.get(requestURL);
     if (response.statusCode == 200) {
       var decodedData = jsonDecode(response.body);
